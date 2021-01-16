e4288fd978400229d90a48f00f1860e73994732a
kurumkan <joomrise@gmail.com>
2021-01-16 02:46:53 +0100

    fix redis url issue

--- config/prod.js
+++ config/prod.js
@@ -3,6 +3,7 @@ module.exports = {
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
+  redisUrl: process.env.REDIS_URL,
   accessKeyId: process.env.AWS_KEY,
   secretAccessKey: process.env.AWS_SECRET
 };
